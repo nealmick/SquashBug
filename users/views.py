65701344436cdaacdd7f9fd44bbac828ac0c8bfa
Neal <33704976+nealmick@users.noreply.github.com>
2022-10-17 19:50:36 -0400

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your Account has been created! You can now login!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your Account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    p = Profile.objects.filter(user=request.user)
+
+    context['ub'] =  p.values('userBugs')[0]['userBugs']
+    context['ob'] =  p.values('userBugs')[0]['userBugs']-p.values('userCompletedBugs')[0]['userCompletedBugs']
+    context['cb'] =  p.values('userCompletedBugs')[0]['userCompletedBugs']
+    context['rb'] =  p.values('redBugs')[0]['redBugs']
+    context['yb'] =  p.values('yellowBugs')[0]['yellowBugs']
+    context['gb'] =  p.values('greenBugs')[0]['greenBugs']
+
+    return render(request, 'users/profile.html', context)
